54026bf53d8712d6fa5c8add06053ae8f0078d63
Vaibhav Srivastava <86321058+VaibhavJeet@users.noreply.github.com>
2023-10-25 16:59:28 +0530

    changes

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
 let title = document.getElementById('main-header');
 title.style.borderBottom = '2px solid black';
 
+//Using Query Selector
+//let additem = document.querySelector('.title');
+// additem.style.fontWeight = 'bold';
+// additem.style.color = 'green';
 
-let additem = document.querySelector('.title');
-additem.style.fontWeight = 'bold';
-additem.style.color = 'green';
+//Using Class Name
+let additem = document.getElementsByClassName('title');
+//Using Index Number
+// additem[0].style.fontWeight = 'bold';
+// additem[0].style.color = 'green';
+
+//Using Loop
+// for(let i = 0; i < additem.length; i++) {
+//     additem[i].style.fontWeight = 'bold';
+//     additem[i].style.color = 'green';
+// }
+
+//Converting to array
+// Array.from(additem).forEach(element => {
+//     element.style.fontWeight = 'bold';
+//     element.style.color = 'green';
+// });
+//OR
+// [...additem].forEach(element => {
+//     element.style.fontWeight = 'bold';
+//     element.style.color = 'green';
+// });
