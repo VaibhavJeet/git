11882c2ae2fe88db25cbd6d3298996e66808f887
Vaibhav Srivastava <86321058+VaibhavJeet@users.noreply.github.com>
2023-10-25 16:39:48 +0530

    commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let title = document.getElementById('main-header');
+title.style.borderBottom = '2px solid black';
+
+
+let additem = document.querySelector('.title');
+additem.style.fontWeight = 'bold';
+additem.style.color = 'green';
